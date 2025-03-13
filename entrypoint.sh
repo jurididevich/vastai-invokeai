52c0e4cc7a683b20f231987421de9cf3da659122
Juri Didevič <38916974+jurididevich@users.noreply.github.com>
2025-03-13 15:10:15 +0100

    v2 entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+# Останавливаем контейнер (если он существует)
+docker stop invokeai || true
+
+# Удаляем контейнер (если он существует)
+docker rm invokeai || true
+
 # Скачиваем нужный образ
 docker pull ghcr.io/ai-dock/invokeai:v2-cuda-11.8.0-base-22.04-v4.2.5
 
-# Запускаем InvokeAI с нужным образом (замените <container_name> на имя вашего контейнера)
+# Запускаем InvokeAI с нужным образом
 docker run -d --name invokeai -p 9090:9090 ghcr.io/ai-dock/invokeai:v2-cuda-11.8.0-base-22.04-v4.2.5
